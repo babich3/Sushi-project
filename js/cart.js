f2759fc5ecb17506c5678eea7ed0c675a7c5d870
babich3 <babicigor720@gmail.com>
2026-03-14 16:08:31 +0200

    Add files via upload

--- js/cart.js
+++ js/cart.js
@@ -37,3 +37,31 @@ function updateCartCount() {
 
     cartCount.textContent = total;
 }
+
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+let current = 0;
+
+function showSlide(index){
+
+slides[current].classList.remove("active");
+
+current = (index + slides.length) % slides.length;
+
+slides[current].classList.add("active");
+
+}
+
+next.addEventListener("click", ()=>{
+
+showSlide(current + 1);
+
+});
+
+prev.addEventListener("click", ()=>{
+
+showSlide(current - 1);
+
+});
